c4a49e7c0c65e41d352b5a07f12640ea96d40bfb
SeasonLeague <facebookups23@gmail.com>
2022-10-16 11:15:30 -0400

    added alternative to python version101

--- Day_001/python_version1.py
+++ Day_001/python_version1.py
@@ -2,3 +2,12 @@ var1 = "Python"
 var2 = "3.8"
 
 print("I love",var1 + " " + var2 + "!")
+
+
+# ALTERNATIVELY, USE THE PRINTF FUNCTION 
+
+language = 'Python'
+version = '3.8'
+ 
+ 
+print(f'I love {language} {version}!')
